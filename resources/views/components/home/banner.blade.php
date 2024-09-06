85be8ff40f9495560bfeabf37fd6b71e1583565b
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-09-06 17:35:14 +0500

    Home Page With Banner

---
+++ resources/views/components/home/banner.blade.php
@@ -0,0 +1,19 @@
+<section
+    class="relative h-screen overflow-hidden font-sans bg-black rounded-lg before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
+    <img src="https://readymadeui.com/cardImg.webp" alt="Banner Image"
+        class="absolute inset-0 object-cover w-full h-full rounded-lg" />
+
+    <div
+        class="relative z-20 flex flex-col items-center justify-center h-full max-w-6xl p-6 mx-auto text-center text-white rounded-lg">
+        <div class="mb-6">
+            <h2 class="text-2xl font-bold sm:text-4xl">Explore the World</h2>
+            <p class="text-base text-center text-gray-200 sm:text-lg">Embark on unforgettable journeys. Book your dream
+                vacation today!</p>
+        </div>
+
+        <button type="button"
+            class="px-6 py-3 mt-12 text-base text-white transition duration-300 bg-transparent border border-white rounded-lg hover:bg-white hover:text-black">
+            Book Now
+        </button>
+    </div>
+</section>

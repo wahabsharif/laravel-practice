6ebc98e2092d8373100b45619a78975f5d813a6a
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-09-06 17:05:40 +0500

    Bot Footer

---
+++ resources/views/components/footer/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="py-4 mt-auto text-white bg-gray-800">
+    <div class="container mx-auto text-center">
+        <p>&copy; {{ date('Y') }} My Laravel App. All rights reserved.</p>
+    </div>
+</footer>
